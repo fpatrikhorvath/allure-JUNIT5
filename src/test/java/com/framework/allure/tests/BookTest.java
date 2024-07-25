c8d9881f369af14644624d71865dce8603c7d148
Patrik Horvath <ferencpatrikhorvath@gmail.com>
2024-07-25 18:33:22 +0200

    replaced TestNg with Junit5, frame of a new test scenario, server.js

--- src/test/java/com/framework/allure/tests/BookTest.java
+++ src/test/java/com/framework/allure/tests/BookTest.java
@@ -4,15 +4,18 @@ import com.framework.allure.core.TestCore;
 import com.framework.allure.rest.response.CreateUser201ResponseDTO;
 import com.framework.allure.rest.response.*;
 import io.qameta.allure.Description;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 import static io.qameta.allure.Allure.step;
-import static org.testng.AssertJUnit.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Execution(ExecutionMode.SAME_THREAD)
 public class BookTest extends TestCore {
 
     @Test
@@ -23,9 +26,21 @@ public class BookTest extends TestCore {
         step("GIVEN a new user of status A", () -> {
             user.set(getUserService().initContextUser("A"));
             final ResponseEntity<CreateUser201ResponseDTO> response = getUserService().registerUser(user.get());
+            Thread.sleep(3 * 1000);
             assertTrue(response.getStatusCode().isSameCodeAs(HttpStatus.CREATED));
         });
+    }
 
+    @Test
+    @Description("Create book for deleted user")
+    public void createBookForDeletedUser() {
+        final AtomicReference<UserDTO> user = new AtomicReference<>();
 
+        step("GIVEN a new user of status A", () -> {
+            user.set(getUserService().initContextUser("A"));
+            final ResponseEntity<CreateUser201ResponseDTO> response = getUserService().registerUser(user.get());
+            Thread.sleep(3 * 1000);
+            assertTrue(response.getStatusCode().isSameCodeAs(HttpStatus.CREATED));
+        });
     }
 }
