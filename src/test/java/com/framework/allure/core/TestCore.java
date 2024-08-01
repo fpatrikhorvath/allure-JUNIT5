98374a57f54a281248c912b89e245a073713a305
Patrik Horvath <ferencpatrikhorvath@gmail.com>
2024-08-01 07:38:34 +0200

    fix for the user layer context store init error

--- src/test/java/com/framework/allure/core/TestCore.java
+++ src/test/java/com/framework/allure/core/TestCore.java
@@ -4,12 +4,14 @@ import com.framework.allure.service.BookService;
 import com.framework.allure.service.UserService;
 import com.framework.allure.stores.UserLayerContextStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration(loader = SpringBootContextLoader.class, value = {"classpath:spring.xml"})
 @SpringBootTest
+@Configurable
 public class TestCore {
 
     @Autowired
